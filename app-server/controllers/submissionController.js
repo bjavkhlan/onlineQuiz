754d7cb3054a5c7c61ba59a49c3a16f95d779e45
bjavkhlan <javkhlan96@gmail.com>
2019-04-21 13:59:14 -0500

    Submission api

--- app-server/controllers/submissionController.js
+++ app-server/controllers/submissionController.js
@@ -4,7 +4,39 @@ const Users = mongoose.model('User');
 const Subjects = mongoose.model('Subject');
 
 
-module.exports.evaluateSubmition = (req, res, next) => {
+module.exports.evaluateSubmition = async (req, res, next) => {
+    const subjectLevel = await Subjects.findOne({"levels._id": req.params.levelId}, {_id: 0, "levels.$.questions": 1}).lean();
+    const questions = subjectLevel.levels[0].questions;
 
-    
+    let correct = 0;
+    const total = questions.length;
+
+    // req.body.answer? 
+    if (total != req.body.answer.length) {
+        next({msg: "data doesn't match"});
+        return;
+    }
+    for (let i = 0; i < total; i++) {
+        if (questions[i].answer.equals(req.body.answer[i].answer)) correct++;
+    }
+
+    const user = await Users.findOne({email: req.user.email});
+    if(!user.levels) user.levels = [];
+    user.levels.push({
+        levelId: new mongoose.mongo.ObjectID(req.params.levelId),
+        gradeForLevel: {
+            correct: correct,
+            total: total
+        }
+    });
+
+    user.save((err) => {
+        if (err) next(err);
+        else {
+            res.json({
+                correct: correct,
+                total: total
+            })
+        }
+    });
 }
